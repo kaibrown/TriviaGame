ba181a7da41bfaa8ca0ffe53b54d248ff7a27dac
Kaisey Brown <kaiseybrown@Kaiseys-MacBook-Air.local>
2017-07-03 22:48:19 -0400

    forgot music

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,26 +3,33 @@ $(document).ready(function(){
 
    
 
-      	 $(".startbutton").on("click", function(){
+   $(".startbutton").on("click", function(){
 
 			 $(".caption").hide();
 	         $(".bgimg").removeClass("bgimg").addClass("newbgimg");
 	         $(".startpage").show();
 	         console.log("the game has begun...now you need to set timer and begin collecting score");
-
-	     
-
-
-
-		});
+			});
   	
+    
 
+      	
 
 
 
 
   	
 
-   });
+   
+
+
+
+
+
+
+
+
+
+ });
 
 
